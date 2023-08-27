fc4be0823e54ae858ca861ed6105c375300eab0b
danny8823 <dannyyoo714@gmail.com>
2023-08-27 09:06:09 -0700

    new navbar

--- frontend/src/NavBar/NavBar.jsx
+++ frontend/src/NavBar/NavBar.jsx
@@ -8,6 +8,7 @@ import { Login } from "../Login/Login";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../Slices/authSlice";
 import newLogo from '../Logo/connect.svg'
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 export const NavBar = () => {
     const loggedIn = useSelector((state) => !!state.me.me.id)
@@ -32,42 +33,50 @@ export const NavBar = () => {
     return (
         <div>
             {loggedIn ? (
-                <div className = 'flex justify-start bg-sky-300 h-16 items-center w-screen m-0'>
-                    <img src = {newLogo} alt = 'connect blog logo' className = 'w-40 h-auto px-10'/>
-                    <Link className = 'px-5 no-underline text-black' to ='/'>Blogs</Link>
-                    <Link className = 'px-5 no-underline text-black' to ='/post'>Post</Link>
-                    <Link className = 'px-5 no-underline text-black' onClick={handleLogout}>Logout</Link>
-                    <Link className = 'px-5 no-underline text-black' to ='/profile'>Profile</Link>
-                </div>
+                <Navbar bg="dark" data-bs-theme="dark">
+                    <Container>
+                    <Navbar.Brand href="#home">Connect Blog</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/post">Post</Nav.Link>
+                        <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                        <Nav.Link href ='/profile'>Profile</Nav.Link>
+                    </Nav>
+                    </Container>
+                </Navbar>
             ): (
-                <div className = 'flex justify-start bg-sky-300 h-16 items-center w-screen m-0'>
-                    <img src = {newLogo} alt = 'connect blog logo' className = 'w-40 h-auto px-10'/>
-                    <Link className = 'px-5 no-underline text-black' to ='/'>Blogs</Link>
-                    <Link className = 'px-5 no-underline text-black' onClick={handleShow}>Login</Link>
-                        <Modal show = {show} onHide={handleClose}>
-                            <Modal.Header closeButton>
-                                <Modal.Title>Login</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body><Login handleClose={handleClose}/></Modal.Body>
-                            <Modal.Footer>
-                                <Button variant = 'secondary' onClick = {handleClose}>
-                                    Close
-                                </Button>
-                            </Modal.Footer>    
-                        </Modal>
-                    <Link className = 'px-5 no-underline text-black' onClick={handleShow2}>Register</Link>
-                        <Modal show = {show2} onHide={handleClose2}>
-                            <Modal.Header closeButton>
-                                <Modal.Title>Register</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body><Register handleClose2={handleClose2}/></Modal.Body>
-                            <Modal.Footer>
-                                <Button variant = 'secondary' onClick = {handleClose2}>
-                                    Close
-                                </Button>
-                            </Modal.Footer>    
-                    </Modal>    
-                </div>
+                <Navbar bg="dark" data-bs-theme="dark">
+                    <Container>
+                        <Navbar.Brand href="#home">Connect Blog</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link onClick={handleShow}>Login</Nav.Link>
+                                <Modal show = {show} onHide={handleClose}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Login</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body><Login handleClose={handleClose}/></Modal.Body>
+                                <Modal.Footer>
+                                        <Button variant = 'secondary' onClick = {handleClose}>
+                                        Close
+                                        </Button>
+                                </Modal.Footer>    
+                                </Modal>
+                            <Nav.Link onClick={handleShow2}>Register</Nav.Link>
+                            <Modal show = {show2} onHide={handleClose2}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Register</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body><Register handleClose2={handleClose2}/></Modal.Body>
+                                <Modal.Footer>
+                                    <Button variant = 'secondary' onClick = {handleClose2}>
+                                        Close
+                                    </Button>
+                                </Modal.Footer>    
+                            </Modal>    
+                        </Nav>
+                    </Container>
+                </Navbar>
             )}   
         </div>
     )
